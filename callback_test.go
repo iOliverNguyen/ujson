cefdc4f80f3eb30a7122b77a935ee39dc9149307
Oliver N <olvrng@gmail.com>
2024-02-15 22:33:54 +0700

    remove kube.go and dependencies

--- callback_test.go
+++ callback_test.go
@@ -370,25 +370,3 @@ func TestArgoApplication(t *testing.T) {
 	// print time taken
 	fmt.Printf("Time taken for json.Unmarshal(): %v\n", time.Since(begin))
 }
-
-func TestEmptyJson(t *testing.T) {
-	input := []byte(`{
-
-	}`)
-	callbacks := []*MatchCallback{}
-	opt := MatchOptions{
-		IgnoreCase:       false,
-		QuitIfNoCallback: false,
-	}
-	res := MatchResult{
-		Count: 0,
-	}
-	err := Match(input, &opt, &res, callbacks...)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// print count
-	if res.Count != 0 {
-		t.Fatal("expected count to be 0")
-	}
-}
